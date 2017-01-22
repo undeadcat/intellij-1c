3fa9e16a4533bb0af2c8176fc3c607713073c703
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2017-01-23 00:17:12 +0300

    fix build script

---
+++ build-deps.sh
@@ -0,0 +1,3 @@
+xbuild simple-1c/Simple1C.sln
+mkdir -p resources/Simple1C
+cp simple-1c/bin/* resources/Simple1C/
--- build.sh
+++
@@ -1,4 +0,0 @@
-xbuild simple-1c/Simple1C.sln
-mkdir -p resources/Simple1C
-cp simple-1c/bin/* resources/Simple1C/
-jar cf com.simple-1c.jar .
